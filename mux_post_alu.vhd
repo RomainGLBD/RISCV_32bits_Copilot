996b6fa430730e4a049ac6f12b1cda0ff47d0a10
GALBARDI Romain <Romain.Galbardi@bordeaux-inp.fr>
2026-02-09 14:41:09 +0100

    commit test bench

--- mux_post_alu.vhd
+++ mux_post_alu.vhd
@@ -15,15 +15,22 @@ architecture Behavioral of mux_post_alu is
 begin
     process(D, alu_out, pc_out, sel_wb)
     begin
+        -- This process selects the write data (WD) based on the value of sel_wb.
+        -- sel_wb is a 2-bit signal that determines the source of the data to be written.
+        -- The selection is as follows:
+        -- "00": Write data (WD) is taken from D.
+        -- "01": Write data (WD) is taken from alu_out (ALU output).
+        -- "10": Write data (WD) is taken from pc_out (Program Counter output).
+        -- others: Write data (WD) is set to zero (default case).
         case sel_wb is
             when "00" =>
-                WD <= D; // BEGIN: MUX Logic
+                WD <= D; 
             when "01" =>
                 WD <= alu_out;
             when "10" =>
                 WD <= pc_out;
             when others =>
-                WD <= (others => '0'); // Default case
-        end case; // END: MUX Logic
+                WD <= (others => '0');
+        end case;
     end process;
 end Behavioral;
